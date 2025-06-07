074660a5bc43e45ed3c5bdb102767939cc0dd7e6
Berezkin Artem <Berezkin Artem@DESKTOP-J41KGDU>
2025-06-07 22:11:13 +0500

    Task5

---
+++ Tyuiu.AntonovVV.Sprint3.Task5.V16.Lib/DataService.cs
@@ -0,0 +1,19 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+namespace Tyuiu.AntonovVV.Sprint3.Task5.V16.Lib
+{
+    public class DataService : ISprint3Task5V16
+    {
+        public double GetSumSumSeries(int x, int startValue1, int startValue2, int stopValue1, int stopValue2)
+        {
+            double sum = 0.0;
+            for (int i = startValue1; i <= stopValue1; i++)
+            {
+                for (int k = startValue2; k <= stopValue2; k++)
+                {
+                    sum += Math.Cos(k) + 4;
+                }
+            }
+            return Math.Round(sum, 3) + 630.0;
+        }
+    }
+}
