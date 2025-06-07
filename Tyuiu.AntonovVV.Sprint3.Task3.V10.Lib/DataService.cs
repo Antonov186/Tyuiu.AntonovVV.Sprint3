2c538d34fe5fa498c8f65c062a9068f6eb7891f1
Berezkin Artem <Berezkin Artem@DESKTOP-J41KGDU>
2025-06-07 21:49:41 +0500

    Task3

---
+++ Tyuiu.AntonovVV.Sprint3.Task3.V10.Lib/DataService.cs
@@ -0,0 +1,18 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+namespace Tyuiu.AntonovVV.Sprint3.Task3.V10.Lib
+{
+    public class DataService : ISprint3Task3V10
+    {
+        public string DeleteCharInString(string value, char item)
+        {
+
+            foreach (char i in value)
+            {
+                if (i == item)
+                {
+                    value = value.Replace(item.ToString(), "");
+                }
+            }
+            return value;
+        }
+    }
---
+++ Tyuiu.AntonovVV.Sprint3.Task3.V10.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.AntonovVV.Sprint3.Task3.V10.Lib;
+namespace Tyuiu.AntonovVV.Sprint3.Task3.V10.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidDeleteCharInString()
+        {
+            DataService ds = new DataService();
+            string value = "gdfppf vfppt p";
+            char item = 'p';
+            string res = ds.DeleteCharInString(value, item);
+            Assert.AreEqual("gdff vft ", res);
+        }
+    }
+}
