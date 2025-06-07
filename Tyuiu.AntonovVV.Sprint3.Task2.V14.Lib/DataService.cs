f04b3003555d412d1ad926564d91c85e27579798
Berezkin Artem <Berezkin Artem@DESKTOP-J41KGDU>
2025-06-07 21:41:52 +0500

    Task2

---
+++ Tyuiu.AntonovVV.Sprint3.Task2.V14.Lib/DataService.cs
@@ -0,0 +1,18 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+namespace Tyuiu.AntonovVV.Sprint3.Task2.V14.Lib
+{
+    public class DataService : ISprint3Task2V14
+    {
+        public double GetMultiplySeries(int value, int startValue, int stopValue)
+        {
+            double MultiplySeries = 1;
+            double i = startValue;
+            do
+            {
+                MultiplySeries = MultiplySeries * Math.Pow(value / i, 3);
+                i++;
+            } while (i <= stopValue);
+            return Math.Round(MultiplySeries, 3);
+        }
+    }
+}
